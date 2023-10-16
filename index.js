343f6986ab7ed3d3eb0c5da50d9134c31fcedb9e
AASJATT <141618407+AASJATT@users.noreply.github.com>
2023-10-16 10:18:30 +0000

    docs: index.js updated

--- index.js
+++ index.js
@@ -1,45 +1,97 @@
 var readlineSync = require('readline-sync');
+
 var score=0;
+
+
+
 
 var userName = readlineSync.question("May I know your name? ");
 
+
+
+
 console.log("Welcome! ",userName," to DO YOU KNOW AASTHA!" );
 
+
+
+
 function play(question,answer){
-  var userAnswer = readlineSync.question(question)
-  if(userAnswer===answer){
+
+  var userAnswer= readlineSync.question(question)
+
+
+  if(userAnswer.toLowerCase()===answer.toLowerCase()){
     console.log("right")
+
     score=score+1
-    
+
+
+
+
   }else{
-    console.log("wrong")}
-  
+
+    console.log("wrong! The correct answer is " , answer)}
+
+
+
+
    console.log("score:",score)}
-  
+
+
+
+
+
+
 
 var questionOne= {
+
   question:"Where do I live? ",
-  answer:"delhi",
-}
-var questionTwo= {
-  question:"What is my favourite food? ",
-  answer:"biryani",
-}
-var questionThree= {
-  question: "Who is my favourite bollywood actor? ",
-  answer:"amir khan"
-}
-var questionFour={
-  question:"what is my favourite bollywood movie? ",
-  answer: "three idiots"
-}
-var questionFive={
-  question:"Who is my favourite singer? ",
-  answer:"arijit singh"
+
+  answer:"Delhi",
+
 }
 
-var questions = [questionOne,questionTwo,questionThree,questionFour,questionFive];
-for(var i=0;i<questions.length;i++){
-  play(questions[i].question,questions[i].answer)
+var questionTwo= {
+
+  question:"What is my favourite food? ",
+
+  answer:"Biryani",
+
 }
+
+var questionThree= {
+
+  question: "Who is my favourite bollywood actor? ",
+
+  answer:"Kartik Aaryan"
+
+}
+
+var questionFour={
+
+  question:"what is my favourite bollywood movie? ",
+
+  answer: "Three Idiots"
+
+}
+
+var questionFive={
+
+  question:"Who is my favourite singer? ",
+
+  answer:"Arijit Singh"
+
+}
+
+
+
+
+var questions = [questionOne,questionTwo,questionThree,questionFour,questionFive];
+
+for(var i=0;i<questions.length;i++){
+
+  play(questions[i].question,questions[i].answer)
+
+}
+
 console.log("Well played! Your final score is: ",score)
